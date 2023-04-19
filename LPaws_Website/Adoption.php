10a19dd3de187ad47d96e50c4c27712567c4ce29
Vladimir Guerrero <vladimirguerrero22@gmail.com>
2023-04-19 18:04:08 +0800

    organized-files

---
+++ LPaws_Website/Adoption.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Strays Adoption</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- CSS -->
+	<style>
+	</style>
+</head>
+
+
+<!-- Webpage Body Start -->
+<body>
+	<!-- Header -->
+	<h1>Lost Paeeeeeeeeeews</h1>
+	
+	<!-- Main Body -->
+	
+	<!-- Footer -->
+	<footer></footer>
+</body>
---
+++ LPaws_Website/Home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- CSS -->
+	<style>
+	</style>
+</head>
+
+
+<!-- Webpage Body Start -->
+<body>
+	<!-- Header -->
+	
+	<!-- Main Body -->
+	
+	<!-- Footer -->
+	<footer></footer>
+</body>
